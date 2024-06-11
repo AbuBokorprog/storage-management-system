bbd49c1c6445c0e76abe804f46a79633ffeb68a2
AbuBokorprog <abubokor1066@gmail.com>
2024-06-11 23:01:57 +0600

    modified and setup

--- dist/app.js
+++ dist/app.js
@@ -3,15 +3,38 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
+const _1 = __importDefault(require("."));
 // import { StudentRoutes } from './modules/students/student.route';
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use((0, cors_1.default)());
 // application routes
-// app.use('/api/v1/student', StudentRoutes);
+app.use(_1.default);
 app.get('/', (req, res) => {
     res.send('Project setup home page');
+});
+// global error
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err, req, res, next) => {
+    const status = 500;
+    const message = err.message || 'Something went wrong!';
+    return res.status(status).json({
+        success: false,
+        message: message,
+        error: err,
+    });
+});
+// notfound route handler
+app.use((req, res, next) => {
+    return res.status(400).json({
+        success: false,
+        message: 'API not found',
+        error: '',
+    });
 });
 exports.default = app;
